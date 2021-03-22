af45bb21d8a4614c7b8f7b06f669efe91a83a4f3
JaMunioz <jamunoz6@miuandes.cl>
2021-03-22 20:57:18 -0300

    primer avance

--- laboratorio-01-JaMunioz.py
+++ laboratorio-01-JaMunioz.py
@@ -1,1 +1,20 @@
-print("hola mundo")
+import random
+j1 = []
+j2 = []
+m1 = []
+m2 = []
+c = int(input("How much card do u want for the players: ")) #both players have the indicated number of the cards 
+for i in range(c):
+    #j1.append(random.randint(1,i)) En caso de generar i cartas al azar, pueden repetirse
+    #j2.append(random.randint(1,i))
+    j1.append(i+1)
+    j2.append(i+1)
+for i in range(c):
+    r = (random.randint(0,c-1-i))
+    m1.append(j1[r])
+    j1.pop(r)
+    r = (random.randint(0,c-1-i))
+    m2.append(j2[r])
+    j2.pop(r)
+print (m1)
+print (m2)
