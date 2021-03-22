11ab360328fc8b114b7d63636f3c3a9497a1f9ec
JaMunioz <jamunoz6@miuandes.cl>
2021-03-22 20:57:30 -0300

    primer avance

--- laboratorio-01-JaMunioz.py
+++ laboratorio-01-JaMunioz.py
@@ -9,12 +9,13 @@ for i in range(c):
     #j2.append(random.randint(1,i))
     j1.append(i+1)
     j2.append(i+1)
-for i in range(c):
+for i in range(c): #randomizer of the presents cards
     r = (random.randint(0,c-1-i))
     m1.append(j1[r])
     j1.pop(r)
     r = (random.randint(0,c-1-i))
     m2.append(j2[r])
     j2.pop(r)
-print (m1)
-print (m2)
+
+p1 = 0
+p2 = 0
