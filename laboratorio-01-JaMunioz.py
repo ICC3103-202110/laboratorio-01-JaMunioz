110164ddc7ba9023e8969565f9a2ba9975b8cf4e
JaMunioz <jamunoz6@miuandes.cl>
2021-03-22 22:41:14 -0300

    tercer avance

--- laboratorio-01-JaMunioz.py
+++ laboratorio-01-JaMunioz.py
@@ -25,17 +25,32 @@ for i in range(c*2):
         else:
             M[0].append(m[i])
 
-print(m)
-print(M)
+print(M[0])
+print(M[1])
+
+while True:
+    while True:
+        A = input("Choose some card using the respective coords (ex: ´0,i´ or ´1,i´ | i length of the row): ")
+        try:  
+            a = int(A.split(",")[0])
+            b = int(A.split(",")[1])
+            if a >= 2 or a <= -1:
+                print("The first number just could be: ´0´ or ´1´")
+            elif b >= c or b <= -1:
+                print("The second number after the ´,´ just can be between ´0´ and "+str(c-1))
+            else:
+                break
+        except :
+            print("if u want continue with this game just use numbers..")
+    break
+
 print(mc)
-A = input("Choose some card using the respective coords (ex: 0,i or 0,i | i length of the row): ")
-a = A.split(",")[0]
-b = A.split(",")[1]
 print(a)
 print(b)
 mc[a].pop(b)
 mc[a].insert(b,M[a][b])
 print(mc)
+
 #points of the players
 p1 = 0 
 p2 = 0
