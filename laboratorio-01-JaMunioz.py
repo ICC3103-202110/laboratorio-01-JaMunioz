5b94874b0616fde4f2a4fc0a005ccf30d18be8fd
JaMunioz <jamunoz6@miuandes.cl>
2021-03-24 01:33:52 -0300

    cuarto avance

--- laboratorio-01-JaMunioz.py
+++ laboratorio-01-JaMunioz.py
@@ -1,11 +1,44 @@
 import random
+
+def Mostrar(matriz):
+    print("")
+    for i in matriz:
+        for k in i:
+            print("\t", k, end=" ")
+        print()
+    print("")
+
+def Girar():
+    while True:
+        while True:
+            A = input("Choose some card using the respective coords (ex: ´0,i´ or ´1,i´ | i length of the row): ")
+            try:  
+                a = int(A.split(",")[0])
+                b = int(A.split(",")[1])
+                if a >= 2 or a <= -1:
+                    print("The first number just could be: ´0´ or ´1´")
+                elif b >= c or b <= -1:
+                    print("The second number after the ´,´ just can be between ´0´ and "+str(c-1))
+                else:
+                    break
+            except :
+                print("if u want continue with this game just use numbers..")
+        break
+    return (a,b)
 j1 = []
 j2 = []
 m = []
-M = [[],[]]
-mc = [[],[]]
 
-c = int(input("How much card do u want for the game: ")) #both players have the indicated number of the cards 
+while True:
+    try:
+        c = int(input("How much pairs of card do you want for the game: ")) #both players have the indicated number of the cards 
+        if c <= 1:
+            print("The numbers of cards need be beetwin 2 pairs and n pairs ")
+        else:
+            break
+    except:
+        print("Error")
+
 for i in range(c):
     j1.append(i+1)
     j2.append(i+1)
@@ -16,43 +49,59 @@ for i in range(c): #randomizer of the presents cards
     r = (random.randint(0,c-1-i))
     m.append(j2[r])
     j2.pop(r)
-    mc[0].append("*")
-    mc[1].append("*")
 
-for i in range(c*2):
-        if i >= c:
-            M[1].append(m[i])
-        else:
-            M[0].append(m[i])
+mm = len(m)
+for i in range(mm):
+    if i*i >= mm:
+        v = i
+        break
 
-print(M[0])
-print(M[1])
+mc = []
+M = []
+r = 0
+for i in range(v):
+    mc.append([])
+    M.append([])
+    for k in range(v):
+        mc[i].append("*")
+        try: 
+            M[i].append(m[r])
+        except:
+            M[i].append(" ")
+        r += 1
+R = []
+for i in range(len(M[0])):
+    R.append(" ")
+print(R)
+print(M)
+print(mc)
 
 while True:
-    while True:
-        A = input("Choose some card using the respective coords (ex: ´0,i´ or ´1,i´ | i length of the row): ")
-        try:  
-            a = int(A.split(",")[0])
-            b = int(A.split(",")[1])
-            if a >= 2 or a <= -1:
-                print("The first number just could be: ´0´ or ´1´")
-            elif b >= c or b <= -1:
-                print("The second number after the ´,´ just can be between ´0´ and "+str(c-1))
-            else:
-                break
-        except :
-            print("if u want continue with this game just use numbers..")
-    break
+    if M[-1] == R:
+        M.pop(-1)
+        mc.pop(-1)
+    else:
+        break
+    
+for i in range(1,len(M[0])+1):
+    if M[-1][-i] == " ":
+        mc[-1].remove("*")
+        mc[-1].append(" ")
+    else:
+        break
 
-print(mc)
-print(a)
-print(b)
+Mostrar(M)
+Mostrar(mc)
+
+a,b = Girar()
 mc[a].pop(b)
 mc[a].insert(b,M[a][b])
-print(mc)
+Mostrar(mc)
 
+a,b = Girar()
+mc[a].pop(b)
+mc[a].insert(b,M[a][b])
+Mostrar(mc)
 #points of the players
 p1 = 0 
 p2 = 0
-
-
